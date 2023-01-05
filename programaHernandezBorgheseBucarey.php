5245fd43af2f433d18d0190652f6fa30784f4352
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2023-01-05 14:22:01 -0300

    cambio

--- programaHernandezBorgheseBucarey.php
+++ programaHernandezBorgheseBucarey.php
@@ -478,7 +478,7 @@ do {
 
         case 5:
 
-            //Para mostrar resumen de jugador  12345
+            //Para mostrar resumen de jugador  1234
             $nombreJugador = solicitarJugador();
 
             resumenDelJugador($nombreJugador, $coleccionPartidas);
